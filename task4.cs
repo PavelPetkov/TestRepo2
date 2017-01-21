9e19b3280f7a5013995001396636a5f52ee267c6
PavelPetkov <ppetkof1983@gmail.com>
2017-01-22 00:43:06 +0200

    Add initial project

---
+++ task4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_and_Variables
+{
+    public class task4
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git Hub");
+        }
+    }
+}
