64f7a8c3b3de5acd9ad27a9abc9b51087dd97822
PavelPetkov <ppetkof1983@gmail.com>
2017-01-22 00:57:38 +0200

    Update task4.cs

--- task4.cs
+++ task4.cs
@@ -13,6 +13,7 @@ namespace Data_Types_and_Variables
             Console.WriteLine("Hello Git Hub");
             Console.WriteLine("Hello from Git Hub 2");
             Console.WriteLine("Hello from Git Hub change");
+            Console.WriteLine("Changed in Github");
         }
     }
 }
